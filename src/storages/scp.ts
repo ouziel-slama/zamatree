42159d49959c57e0cc36dae9fec7fabd5cdfe2f5
Ouziel Slama <ouziel@stashblox.com>
2023-09-23 16:58:18 +0200

    dry and fix scp module

--- src/storages/scp.ts
+++ src/storages/scp.ts
@@ -4,9 +4,9 @@ const { spawn } = require('node:child_process');
 
 import  { SERVERS } from '../config';
 
-const upload = async (folderPath: string, serverName: string) => {
+const scpCommand = async (serverName: string, src: string, dest: string) => {
     if (!SERVERS[serverName]) throw new Error(`Server not found: ${serverName}`);
-    if (!fs.statSync(folderPath).isDirectory()) throw new Error('Path is not a directory: ' + folderPath);
+
     var command = ["-r"];
     if (SERVERS[serverName].port) { 
         command.push(`-P`);
@@ -16,40 +16,30 @@ const upload = async (folderPath: string, serverName: string) => {
         command.push(`-i`);
         command.push(SERVERS[serverName].key);
     };
-    command.push(`${folderPath}`);
-    command.push(`${SERVERS[serverName].host}:${SERVERS[serverName].path}`);
+    command.push(src);
+    command.push(dest);
     //console.log(command.join(' '));
     const scp = spawn('scp', command);
-    scp.stdin = process.stdin;
-    scp.stdout = process.stdin;
-    scp.stderr = process.stdin;
     await new Promise((resolve) => {
+        scp.stdin = process.stdin;
+        scp.stdout.on('data', (data: any) => console.log(data.toString()));
+        scp.stderr.on('data', (data: any) => console.log(data.toString()));
         scp.on('close', resolve);
     });
 }
 
+const upload = async (folderPath: string, serverName: string) => {
+    if (!fs.statSync(folderPath).isDirectory()) throw new Error('Path is not a directory: ' + folderPath);
+    const src = folderPath;
+    const dest = `${SERVERS[serverName].host}:${SERVERS[serverName].path}`;
+    await scpCommand(serverName, src, dest);
+}
+
 const download = async (destFolder: string, fileName: string, serverName: string) => {
-    if (!SERVERS[serverName]) throw new Error(`Server not found: ${serverName}`);
     if (!fs.statSync(destFolder).isDirectory()) throw new Error('Path is not a directory: ' + destFolder);
-    var command = [];
-    if (SERVERS[serverName].port) { 
-        command.push(`-P`);
-        command.push(SERVERS[serverName].port)
-    };
-    if (SERVERS[serverName].key) {
-        command.push(`-i`);
-        command.push(SERVERS[serverName].key);
-    };
-    command.push(`${SERVERS[serverName].host}:${SERVERS[serverName].path}/${fileName}`);
-    command.push(`${destFolder}/${path.basename(fileName)}`);
-    //console.log(command.join(' '));
-    const scp = spawn('scp', command);
-    scp.stdin = process.stdin;
-    scp.stdout = process.stdin;
-    scp.stderr = process.stdin;
-    await new Promise((resolve) => {
-        scp.on('close', resolve);
-    });
+    const src = `${SERVERS[serverName].host}:${SERVERS[serverName].path}/${fileName}`;
+    const dest = `${destFolder}/${path.basename(fileName)}`;
+    await scpCommand(serverName, src, dest);
 }
 
 const scp = module.exports = {
