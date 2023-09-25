fe013b893cc154b8bd4276f3f79b377067b7fada
Ouziel Slama <ouziel@stashblox.com>
2023-09-25 16:08:16 +0200

    fix on linux

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const appFolder = path.join(os.homedir(), ".zamatree");
 const serversFile = path.join(appFolder, "servers.json");
